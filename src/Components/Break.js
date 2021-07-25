62478bbb7b68ffa92fd24c74ce26e903815d3194
untari <sastraembun0306@gmail.com>
2021-07-25 16:25:21 +0700

    add plus and minus button ith click event handlers

--- src/Components/Break.js
+++ src/Components/Break.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
 
+
 const Break = () => {
-    const [breakLengthInSeconds, setBreakLengthInSecond] = useState(300);
-  return (
-    <div>
-      <p id="break-label">Break</p>
-      <p id="break-length">{breakLengthInSeconds}</p>
-    </div>
-  );
+    //Initialize break length using useState
+    const [breakLengthInSeconds, setBreakLengthInSeconds] = useState(300);
+    
+    const decrementBreakLengthByOneMinute = () => {
+        const newBreakLengthInSeconds = breakLengthInSeconds - 6;
+        if (newBreakLengthInSeconds < 0) {
+            setBreakLengthInSeconds(0);
+        } else {
+            setBreakLengthInSeconds(newBreakLengthInSeconds);
+        }
+    };
+    
+    const incrementBreakLengthByOneMinute = () => setBreakLengthInSeconds(breakLengthInSeconds + 60);
+
+    return (
+        <div>
+        <p id="break-label">Break</p>
+        <p id="break-length">{breakLengthInSeconds}</p>
+        <button id="break-increment" onClick={incrementBreakLengthByOneMinute}> + </button>
+        <button id="break-decrement" onClick={decrementBreakLengthByOneMinute}> - </button>
+        </div>
+    );
 };
 export default Break;
