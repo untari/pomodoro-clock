f09b629744683252e2509f641e3fefff23ce07a1
untari <sastraembun0306@gmail.com>
2021-07-25 23:37:38 +0700

    create session function component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Break from './Components/Break';
+import Session from './Components/Session';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Break />
+        <Session />
     </div>
   );
 }
---
+++ src/Components/Session.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import moment from 'moment'
+
+const Session = () => {
+    return(
+        <div>
+            <p id="session-label">Session</p>
+        </div>
+    );
+};
+
+export default Session;
