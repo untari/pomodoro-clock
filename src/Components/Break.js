6a42e21cda39ec668195c2978bee774b5114c9e4
untari <sastraembun0306@gmail.com>
2021-07-25 14:46:38 +0700

    Initialize break length using react useState

---
+++ src/Components/Break.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Break = () => {
+    const [breakLengthInSeconds, setBreakLengthInSecond] = useState(300);
+  return (
+    <div>
+      <p id="break-label">Break</p>
+      <p id="break-length">{breakLengthInSeconds}</p>
+    </div>
+  );
+};
+export default Break;
