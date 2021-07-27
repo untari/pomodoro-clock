c54842021fee546b95c83ac65961773776c85c5e
untari <sastraembun0306@gmail.com>
2021-07-27 11:37:32 +0700

    Initialize session length using react useState

--- src/Components/Session.js
+++ src/Components/Session.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import moment from 'moment'
 
 const Session = () => {
+    const [sessionLengthInSeconds, setSessionLengthInSeconds] useState(60 * 25);
     return(
         <div>
             <p id="session-label">Session</p>
+            <p id="session-length">{sessionLengthInSeconds}</p>
         </div>
     );
 };
